fa35e6a81a367c2a0612d8a43a2e4d1204c8ce78
shaery <shaery.parsha@gmail.com>
2021-10-04 19:17:52 +0600

    my fifth commit

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404 Not Found</h2>
-            <h4>Amake khuje paba na.</h4>
+        <div className="text-center m-4">
+            <img src="https://img-premium.flaticon.com/png/512/4196/premium/4196429.png?token=exp=1633354198~hmac=a34b15ac64f54e9d276ef9177c700fb0" alt="404_not_found" /> <br />
+            <h1>Not Found</h1>
         </div>
     );
 };
